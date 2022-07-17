43475ef445f30280df1c9fff2f6e36d7d33e4267
stargazer39 <dehemiweerakkodi2@gmail.com>
2022-07-17 20:14:49 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+from fastapi import FastAPI
+import gunicorn
+
+ceb_home = "https://cebcare.ceb.lk/Incognito/DemandMgmtSchedule"
+ceb_power_cut_details_endpoint = "https://cebcare.ceb.lk/Incognito/GetLoadSheddingGeoAreas"
+
+app = FastAPI()
+r_session = requests.Session()
+
+def get_verification_token():
+    html = r_session.get(ceb_home, verify=False).text
+    soup = BeautifulSoup(html, 'html.parser')
+    token = soup.find("input", attrs={ "name": "__RequestVerificationToken" })
+    return token.attrs.get("value")
+
+def get_power_cut_details(group_letter: str, token: str):
+    res = r_session.get(ceb_power_cut_details_endpoint, 
+            params={
+                "LoadShedGroupId":group_letter.capitalize()
+            },
+            headers={
+                "requestverificationtoken": token
+            },
+            verify=False
+        )
+    
+    string = res.content.decode('unicode_escape')
+    return json.loads(string[1:-1])
+
+
+@app.get("/power_cuts/{group}")
+async def power_cuts(group):
+    token = get_verification_token()
+    details = get_power_cut_details(group, token)
+    return details
