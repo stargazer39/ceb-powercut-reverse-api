9c7873782d1d3a6e22c33905d332f14e73012e8c
stargazer39 <dehemiweerakkodi2@gmail.com>
2022-07-17 21:50:49 +0530

    Correct endpoints and responses

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
+from datetime import datetime, timedelta
+import pytz
+from boltons.iterutils import remap
+
 import gunicorn, uvicorn
 
 ceb_home = "https://cebcare.ceb.lk/Incognito/DemandMgmtSchedule"
 ceb_power_cut_details_endpoint = "https://cebcare.ceb.lk/Incognito/GetLoadSheddingGeoAreas"
+ceb_load_shedding_events_endpoint = "https://cebcare.ceb.lk/Incognito/GetLoadSheddingEvents"
 
 app = FastAPI()
 r_session = requests.Session()
 
+drop_falsey = lambda path, key, value: bool(value)
 def get_verification_token():
     html = r_session.get(ceb_home, verify=False).text
     soup = BeautifulSoup(html, 'html.parser')
@@ -30,9 +36,34 @@ def get_power_cut_details(group_letter: str, token: str):
     string = res.content.decode('unicode_escape')
     return json.loads(string[1:-1])
 
+def get_all_power_cut_events(token: str):
+    today = datetime.now(pytz.timezone("Asia/Colombo"))
+    next_day = today + timedelta(days=1)
+    
+    res = r_session.post(ceb_load_shedding_events_endpoint, 
+            data={
+                "StartTime": today.date(),
+                "EndTime": next_day.date()
+            },
+            headers={
+                "requestverificationtoken": token
+            },
+            verify=False
+        )
+    return res.json()
 
-@app.get("/group/{group}/power_cuts")
-async def power_cuts(group):
+@app.get("/power_cuts")
+async def power_cuts():
     token = get_verification_token()
-    details = get_power_cut_details(group, token)
-    return details
+    details = get_all_power_cut_events(token)
+    return remap(details, visit=drop_falsey)
+
+@app.get("/group/{group}/power_details")
+async def power_cuts(group: str):
+    token = get_verification_token()
+    details = get_all_power_cut_events(token)
+
+    for power_details in details:
+        if 'loadShedGroupId' in power_details and power_details['loadShedGroupId'] == group.capitalize():
+            return remap(power_details, visit=drop_falsey)
+    return {}
